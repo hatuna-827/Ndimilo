ee5addb808dca1bb7b0f59e45b8a879060762651
hatuna-827 <hatuna827yadaka@gmail.com>
2026-07-06 22:06:50 +0900

    add glad & glfw

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,45 @@
-#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-  std::cout << "Hello Ndimilo!" << std::endl;
+  if (!glfwInit())
+  {
+    return -1;
+  }
+
+  GLFWwindow *window =
+      glfwCreateWindow(
+          800,
+          600,
+          "Ndimilo Renderer",
+          nullptr,
+          nullptr);
+
+  if (!window)
+  {
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    return -1;
+  }
+
+  while (!glfwWindowShouldClose(window))
+  {
+    glClearColor(0, .3, 0, 1);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+
   return 0;
 }
